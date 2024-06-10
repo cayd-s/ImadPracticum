affcd7374448571095cdd92c56f3829a94e2c864
cayd-s <symonscayden@gmil.com>
2024-06-10 09:33:48 +0200

    changes

--- app/src/main/java/com/example/imadpracticum/MainScreenActivity.kt
+++ app/src/main/java/com/example/imadpracticum/MainScreenActivity.kt
@@ -1,34 +1,20 @@
 package com.example.imadpracticum
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity<Button> : AppCompatActivity() {
+class MainScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val mainScreenButton = findViewById<android.widget.Button>(R.id.mainScreenButton)
-        mainScreenButton.setOnClickListener {
-            val intent = Intent(this, MainScreenActivity::class.java)
-            startActivity(intent)
-        }
-
+        setContentView(R.layout.activity_main_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val exitButton = findViewById<android.widget.Button>(R.id.exitButton)
-        exitButton.setOnClickListener {
-            finish()
         }
     }
 }
