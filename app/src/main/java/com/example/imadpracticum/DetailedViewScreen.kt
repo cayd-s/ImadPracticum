e06a50b67b42217bf8b9b25169e04767c02ed3ec
cayd-s <symonscayden@gmil.com>
2024-06-10 13:18:50 +0200

    changes

--- app/src/main/java/com/example/imadpracticum/DetailedViewScreen.kt
+++ app/src/main/java/com/example/imadpracticum/DetailedViewScreen.kt
@@ -3,12 +3,17 @@ package com.example.imadpracticum
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class DetailedViewScreen : AppCompatActivity() {
+
+    val days = arrayOf("Sunday: 10-20 Sunny", "Monday: 11-21 Cloudy", "Tuesday: 12-22 Windy", "Wednesday: 13-23 Rainy", "Thursday: 14-24 Windy", "Friday: 15-25 Cloudy", "Saturday: 16-26 Sunny")
+
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,5 +30,19 @@ class DetailedViewScreen : AppCompatActivity() {
         thirdExitButton.setOnClickListener {
             finish()
         }
+        val displayTextView = findViewById<TextView>(R.id.displayTextView)
+
+        var detailsDisplay = ""
+        var counter = 0
+        while (counter < days.size) {
+            detailsDisplay += "${days[counter]}\n"
+
+            counter++
+            displayTextView.text = detailsDisplay
+
+
+            }
+
+        }
+
     }
-}
--- app/src/main/java/com/example/imadpracticum/MainScreenActivity.kt
+++ app/src/main/java/com/example/imadpracticum/MainScreenActivity.kt
@@ -1,8 +1,10 @@
 package com.example.imadpracticum
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,8 +12,17 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainScreenActivity : AppCompatActivity() {
 
-    val days = arrayOf<String>("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+    val days = arrayOf<String>(
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    )
     val temperature = arrayOf<Int>(20, 21, 22, 23, 24, 25, 26)
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,7 +30,7 @@ class MainScreenActivity : AppCompatActivity() {
 
         val detailedViewButton = findViewById<android.widget.Button>(R.id.detailedViewButton)
         detailedViewButton.setOnClickListener {
-            val intent = Intent(this,DetailedViewScreen::class.java)
+            val intent = Intent(this, DetailedViewScreen::class.java)
             startActivity(intent)
         }
 
@@ -32,8 +43,41 @@ class MainScreenActivity : AppCompatActivity() {
         val secondExitButton = findViewById<Button>(R.id.secondExitButton)
         secondExitButton.setOnClickListener {
             finish()
+
+
         }
 
+        val inputEditText = findViewById<TextView>(R.id.inputEditText)
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        val getTempButton = findViewById<Button>(R.id.getTempButton)
+        getTempButton.setOnClickListener {
+            val input = inputEditText.text.toString()
+            val dayIndex = days.indexOf(input)
+            if (dayIndex != -1) {
+                resultTextView.text = "Temperature on ${days[dayIndex]} is ${temperature[dayIndex]}°C"
+            } else {
+                resultTextView.text = "Invalid input, Please use capital letters"
+            }
+        }
+        val averageTempTextView = findViewById<TextView>(R.id.averageTempTextView)
+        val getAverageButton = findViewById<Button>(R.id.getAverageButton)
+        getAverageButton.setOnClickListener {
+            val sum = temperature.sum()
+            val average = sum / temperature.size
+            averageTempTextView.text = "Average temperature this week is $average°C"
+        }
+        val clearButton = findViewById<Button>(R.id.clearButton)
+        clearButton.setOnClickListener {
+            inputEditText.text = ""
+            resultTextView.text = ""
+            averageTempTextView.text = ""
+
+
+        }
 
     }
+
 }
+
+
+
